987172d3a2c55526f7f677ae11fbaea84b0ad325
Toan Bui (Flexer) <bnqtoan@gmail.com>
2021-09-13 19:47:26 +0700

    update live

--- 03/tailwindcss/tailwind.config.js
+++ 03/tailwindcss/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      "heading": "Arial, sans"
+    },
     extend: {
       container: {
         center: true
